10dc0b471aeab74bfa72c1ee5460ea9c1bce8e3d
cbm3 <ch.bmmoreira@gmail.com>
2022-07-18 21:00:08 -0300

    Colisão e game over.

--- script.js
+++ script.js
@@ -46,6 +46,14 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
 
+
+    for (i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -74,4 +82,4 @@ function iniciarJogo() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 500);
+let jogo = setInterval(iniciarJogo, 300);
